c76a8f394459b5b0b15130068f80d4b58a76c675
Carlos <mountaindew7436@gmail.com>
2023-03-02 14:50:28 +0000

    Register page started

---
+++ src/register.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="css/index.css" rel="stylesheet" type="text/css">
+    <link href="css/register.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+</br>
+    <div class="container">
+        <div class="row">
+            <div class="d-flex justify-content-between"> WiredIn
+                <div>
+                    <button type="button" class="btn"> Login </button> 
+                </div>
+            </div>
+        </div>
+        </br>
+        <div class="row">
+            <div class="d-flex justify-content-center">
+                <button type="button" class="btn_2 btn-outline-light btn-lg" aria-pressed="true"> Company </button>
+            </div>
+        </div>
+        </br>
+        <div class="row">
+            <div class="d-flex justify-content-center">
+                <button type="button" class="btn_2 btn-outline-light btn-lg" aria-pressed="true"> Company </button>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
