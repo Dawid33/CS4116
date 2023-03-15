57cc6daa768caa3c8ab7c7382a9d31aaf3ae980c
Dawid Sobczak <dawidsobczak@fastmail.com>
2023-03-15 18:54:32 +0000

    fixed search page buttons

--- src/generators/search/gen_search_content.php
+++ src/generators/search/gen_search_content.php
@@ -12,10 +12,17 @@
     <input type="radio" name="options" id="users" autocomplete="off"> Users
   </label>
   <form class="d-flex" role="search" action="search.php" >
-    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search-term" value="<?php print $_GET['search-term'] ?>">
-    <button class="btn btn-outline-success" type="submit">Search</button>
+    <input class="form-control me-2" id="search-field" type="search" placeholder="Search" aria-label="Search" name="search-term" value="<?php print $_GET['search-term'] ?>">
+    <a class="btn btn-outline-success" id="search-submit-button">Search</a>
 </form>
 </div>
 
 <div id="results-pane">
+  <?php 
+    $_GET['search-type'] = 'vacancies';
+    if (!$_GET['serach-term']) {
+      $_GET['search-term'] = '';
+    }
+    include("search_api.php");
+  ?>
 </div>
--- src/js/search.js
+++ src/js/search.js
@@ -12,16 +12,24 @@ function replace_results_with_api_call(url) {
     });
 }
 
-
-
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-const search_term = urlParams.get('search-term');
-replace_results_with_api_call(`http://localhost/generators/search/search_api.php?search-type=vacancies&search-term=${search_term}`);
+var search_term = urlParams.get('search-term');
+var search_type = 'vacancies';
+// replace_results_with_api_call(`http://localhost/generators/search/search_api.php?search-type=vacancies&search-term=${search_term}`);
 
 function with_search_type(type) {
+    search_type = type;
     replace_results_with_api_call(`http://localhost/generators/search/search_api.php?search-type=${type}&search-term=${search_term}`);
 }
+
+let submit_button = document.getElementById('search-submit-button');
+let submit_field = document.getElementById('search-field');
+
+submit_button.addEventListener("click", (e) => {
+    search_term = submit_field.value;
+    replace_results_with_api_call(`http://localhost/generators/search/search_api.php?search-type=${search_type}&search-term=${search_term}`);
+});
 
 vacancies.addEventListener("click", ((e) => { with_search_type("vacancies")}));
 users.addEventListener("click", ((e) => { with_search_type("users")}));
