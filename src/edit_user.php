ff2c821b912a9f9012404c2d04b132b2ff3af48e
TZajas <20278748@studentmail.ul.ie>
2023-03-21 17:19:26 +0000

    refined create_org and edit forms

--- src/edit_user.php
+++ src/edit_user.php
@@ -4,6 +4,7 @@
        header("Location: login.php");
     } 
     $current_user_id = $_SESSION["user"];
+    $edit_errors = array();
 
     if (isset($_POST["save"])) {
         $conn = new mysqli("db", "cs4116", "cs4116", "cs4116");
@@ -14,7 +15,7 @@
         }   
 
         $first_name = mysqli_real_escape_string($conn, $_POST["first_name"]);
-        $last_name = mysqli_real_escape_string($conn, $_POST["first_name"]);
+        $last_name = mysqli_real_escape_string($conn, $_POST["last_name"]);
         $bio = mysqli_real_escape_string($conn, $_POST["bio"]);  
 
         if (empty($first_name) || empty($last_name)) {
@@ -23,19 +24,18 @@
             $error=false;
         }
 
-        // if (!$error) {
-            $sql_update= "UPDATE users SET first_name='$first_name', last_name='$last_name', bio='$bio' WHERE user_id = '$current_user_id'";
+        if (!$error) {
+            $sql_update = "UPDATE users SET first_name='$first_name', last_name='$last_name', bio='$bio' WHERE user_id = '$current_user_id'";
 
             if (($conn->query($sql_update))) {
                 header("Location: user.php?id=".$current_user_id);
-                
             }else {
                 echo $conn->error;
             }
-        // // }else{
-        //     $_SESSION["edit_profile_error"]=$edit_profile_error;
-        //     header("Location: edit_user.php?error=" . "error");
-        // }
+        } else {
+            $edit_errors["fieldsValidation"] = "Full name is required";
+        }
+
         $conn->close();
     }
 ?>
@@ -50,7 +50,6 @@
 
 <body>
 </br>
-<!-- action="index.php" -->
     <div class="container">
         <div class="row">
             <div class="d-flex justify-content-between display-6"> WiredIn 
@@ -65,7 +64,7 @@
         </br>
         <div class="row">
             <div class= "d-md-flex justify-content-center"> 
-                <form method="post">
+                <form action="edit_user.php" method="post">
                     <div class="form-group">
                         <input type="text" name="first_name" class="form-control" placeholder="First Name" value="<?php echo htmlspecialchars($_POST['first_name'] ?? '', ENT_QUOTES);?>">
                     </div>
@@ -82,7 +81,7 @@
                         <input class="btn btn-lg" type="submit" name="save" value="Save"></input>
                     </div>
                     </br>
-                    <?php if(isset($_GET['error'])){ print "<div class='text-center alert alert-danger'>Name is required</div>"; }?>
+                    <?php if(array_key_exists("fieldsValidation", $edit_errors)){ $err = $edit_errors["fieldsValidation"]; print "<div class='text-center alert alert-danger'>$err</div>"; }?>
                 </form>
             </div>
         </div>
