84bca55cc1bb95a37ea4d69cae51d600ac5f4b86
Clevercael1 <Caeloflaherty@gmail.com>
2023-03-22 02:29:32 +0000

    Added is_admin to session

--- src/index.php
+++ src/index.php
@@ -2,7 +2,11 @@
     session_start();
     if (!isset($_SESSION["user"])) {
        header("Location: login.php");
-    } 
+    }
+    
+    if (!isset($_SESSION["user_is_admin"])) {
+        header("Location: login.php");
+     }
     $current_user_id = $_SESSION["user"];
 
     $content_php_file = "index/gen_index_content.php";
