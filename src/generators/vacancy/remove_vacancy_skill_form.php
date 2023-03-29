34e7411eee734246282ca3c8dcf643e80e24ae23
Clevercael1 <Caeloflaherty@gmail.com>
2023-03-29 03:05:17 +0100

    Fixed issue deleting skill from vacancy is random

--- src/generators/vacancy/remove_vacancy_skill_form.php
+++ src/generators/vacancy/remove_vacancy_skill_form.php
@@ -1,11 +1,3 @@
-<?php
-$vacancySql = 'SELECT * FROM vacancy_skills WHERE vacancy_id = "' . $vacancy_id . '"';
-$vacancyResult = $conn->query($vacancySql);
-while ($row = $vacancyResult->fetch_assoc()) {
-    $vacancy_skill_id = $row["vacancy_skill_id"];
-}
-?>
-
 <form action="remove_vacancy_skill.php" method="post">
     <input style="display:none;" name="vacancy_skill_id" value="<?php echo $vacancy_skill_id; ?>" type="text"> </input>
     <input style="display:none;" name="vacancy_id" value="<?php echo $vacancy_id; ?>" type="text"> </input>
