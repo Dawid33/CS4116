5bbd0cc5311e1532d4573215e0ce432fa8f273eb
Clevercael1 <Caeloflaherty@gmail.com>
2023-04-20 02:37:54 +0100

    Fixed qualification description going off screen

--- src/generators/user/qualification.php
+++ src/generators/user/qualification.php
@@ -1,6 +1,6 @@
 <tr class="qual-list list-group-item justify-content-between d-flex">
     <td class="qual-title"> <b> <?php echo $title; ?> </b> </td>
-    <td> <?php echo $description; ?> </td>
+    <td style="width: 65%;"> <?php echo $description; ?> </td>
     <td class="mr-auto p-2">
         <?php
         if (strcmp($user_id, $current_user_id) == 0 || $_SESSION["user_is_admin"] == 1) {
