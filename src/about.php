98e23921f266de3d050b2ac7956ab172afd05e43
Clevercael1 <Caeloflaherty@gmail.com>
2023-04-22 17:19:23 +0100

    Fix for link to profile not working

--- src/about.php
+++ src/about.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+$current_user_id = $_SESSION["user"];
     $content_php_file = "gen_about_content.php";
     $head_content = "<link href=\"css/index.css\" rel=\"stylesheet\" type=\"text/css\">";
     include('generators/generic.php');
